7ea9b0e27ef06be5c5d4cef9730a5dbc6cb1a23e
lbalbrecht <lalbrecht18@wooster.edu>
2021-04-06 14:14:31 -0700

    ingredient selector mostly complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
-var mainWindow = document.querySelector("#main-window");
+var mainWindow = document.querySelector("#landing-page");
 var searchByRecipes = document.querySelector("#recipeToSearch");
 var searchRecBtn = document.querySelector("#input-field");
+var recipSchBtn = document.querySelector("#recipe-search-btn")
+var ingredSchBtn = document.querySelector("#ingredient-search-btn")
+var ingredientSearch = document.querySelector("#ingredient-search")
 
 //HTML / CSS
 //Utilize Materialize CSS framework for page design and layout
@@ -34,117 +37,180 @@ var searchRecBtn = document.querySelector("#input-field");
 // displayRecipeOptions()
 // generate div content for page with description of choices between searching for a recipe by name or searching by ingredients you have in your pantry. creates two buttons that lead to the next set of functions
 
+ingredSchBtn.addEventListener("click", function(event) {
+  event.preventDefault();
+  mainWindow.classList.add("hide")
+  ingredientSearch.classList.remove("hide")
+})
 // thePantryPage()
 // generates a description of the pantry page and how it functions, includes input form for ingredients, calls savePantry on submission and Update Pantry to populate items
 
-// savePantry()
+function savePantry() {
+  localStorage.setItem("pantry", pantry)
+}
 // allows the user to add food items to their pantry list and save them to local storage
 
 // updatePantry()  and removeItem()??
 //updates/generates the list of user pantry items on screen, also used to remove items from the list
+var pantryFormEl = document.querySelector('#ingredients-add');
+var pantryListEl = document.querySelector('#ingredients-list');
+var ingredientBtnEl = document.querySelector('#add-ingredient-btn');
+var pantry = [];
 
-// recipeSearch()
-//initially low functionality that will be built out with options if time permits, generates description and input form for search term and button to submit
-function recipeSearch() {
-    //create a div row for the page description
-    mainWindow.innerHTML="";
-  var newRow = document.createElement("div");
-  newRow.classList = "row center-align";
-  mainWindow.appendChild(newRow);
-  var description = document.createElement("p");
-  description.textContent =
-    "Looks like you already have a recipe in mind that you would like to make for dinner! In that case, all you need to do is put the name of recipe you'd like to find in the field below and hit the Search button!";
-  description.classList = "col s6 offset-s3";
-  newRow.appendChild(description);
+// document.addEventListener('DOMContentLoaded', function() {
+//   var elems = document.querySelectorAll('.chips');
+//   var instances = M.Chips.init(elems, data);
+// });
 
-  //create a second div row for the inputform
-  var secRow = document.createElement("div");
-  secRow.classList = "row center-align";
-  mainWindow.appendChild(secRow);
+// function to handle form submission
+ingredientBtnEl.addEventListener('click', function(event){
 
-  //create a form element
-  var inputForm = document.createElement("form");
-  secRow.appendChild(inputForm);
-
-  //create a text input field and add it to the form
-  var inputField = document.createElement("div");
-  inputField.classList = "input-field col s6 offset-s3";
-  inputForm.appendChild(inputField);
-  var textInput = document.createElement("input");
-  textInput.type = "text";
-  textInput.id = "recipeToSearch";
-  textInput.classList = "validate";
-  inputField.appendChild(textInput);
-
-//create a label for the text field and add it to the form
-  var inputLabel = document.createElement("label");
-  inputLabel.setAttribute("for", "recipeToSearch");
-  inputLabel.textContent = "Find this recipe";
-  inputField.appendChild(inputLabel);
-
-  //create a button and add it to the input form
-  var subBtn = document.createElement("button");
-  subBtn.classList = "btn waves-effect waves-light col s6 offset-s3";
-  subBtn.type = "submit";
-  subBtn.setAttribute("id", "recipeSearchButton");
-  subBtn.textContent = "Search";
-  inputForm.appendChild(subBtn);
-}
-
-recipeSearch();
-
-mainWindow.addEventListener("submit", function (event) {
   event.preventDefault();
+
+  var ingredient = pantryFormEl.value;
+  console.log(pantryFormEl.value);
+
+  if (!ingredient) {
+    console.log('No shopping item filled out in form!');
+    return;
+  }
+
+  var listItemEl = document.createElement("li");
+  listItemEl.textContent = ingredient;
+
+  // var deleteBtnEl = document.createElement("button");
+
+  // deleteBtnEl.textContent = "x";
+
+  // deleteBtnEl.addEventListener("click", function(event){
+  //   event.preventDefault();
+  //   pantryListEl.removeChild(listItemEl);
+  //   pantry.splice(pantry.indexOf[listItemEl])
+  //   console.log(pantry)
+  // })
+
+  // listItemEl.appendChild(deleteBtnEl);
+
+  pantryListEl.appendChild(listItemEl);
+
+  pantry.push(ingredient);
+
+pantryFormEl.value = ('');
+
+console.log(pantry)
+
+savePantry();
+
+})
+
+
+
+// Create a submit event listener on the form element
+// handleFormSubmit(event));
+
+
+// // recipeSearch()
+// //initially low functionality that will be built out with options if time permits, generates description and input form for search term and button to submit
+// function recipeSearch() {
+//     //create a div row for the page description
+//     mainWindow.innerHTML="";
+//   var newRow = document.createElement("div");
+//   newRow.classList = "row center-align";
+//   mainWindow.appendChild(newRow);
+//   var description = document.createElement("p");
+//   description.textContent =
+//     "Looks like you already have a recipe in mind that you would like to make for dinner! In that case, all you need to do is put the name of recipe you'd like to find in the field below and hit the Search button!";
+//   description.classList = "col s6 offset-s3";
+//   newRow.appendChild(description);
+
+//   //create a second div row for the inputform
+//   var secRow = document.createElement("div");
+//   secRow.classList = "row center-align";
+//   mainWindow.appendChild(secRow);
+
+//   //create a form element
+//   var inputForm = document.createElement("form");
+//   secRow.appendChild(inputForm);
+
+//   //create a text input field and add it to the form
+//   var inputField = document.createElement("div");
+//   inputField.classList = "input-field col s6 offset-s3";
+//   inputForm.appendChild(inputField);
+//   var textInput = document.createElement("input");
+//   textInput.type = "text";
+//   textInput.id = "recipeToSearch";
+//   textInput.classList = "validate";
+//   inputField.appendChild(textInput);
+
+// //create a label for the text field and add it to the form
+//   var inputLabel = document.createElement("label");
+//   inputLabel.setAttribute("for", "recipeToSearch");
+//   inputLabel.textContent = "Find this recipe";
+//   inputField.appendChild(inputLabel);
+
+//   //create a button and add it to the input form
+//   var subBtn = document.createElement("button");
+//   subBtn.classList = "btn waves-effect waves-light col s6 offset-s3";
+//   subBtn.type = "submit";
+//   subBtn.setAttribute("id", "recipeSearchButton");
+//   subBtn.textContent = "Search";
+//   inputForm.appendChild(subBtn);
+// }
+
+// recipeSearch();
+
+// mainWindow.addEventListener("submit", function (event) {
+//   event.preventDefault();
   
-  console.log(document.getElementById("recipeToSearch").value);
-  getRecipes(document.getElementById("recipeToSearch").value);
-});
-//getRecipes()
-// calls spoonacular api for recipes based on key term search and returns object
-function getRecipes(searchTerm) {
+//   console.log(document.getElementById("recipeToSearch").value);
+//   getRecipes(document.getElementById("recipeToSearch").value);
+// });
+// //getRecipes()
+// // calls spoonacular api for recipes based on key term search and returns object
+// function getRecipes(searchTerm) {
 
-    fetch(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?query=${searchTerm}&number=10&offset=0`, {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-key": "0f2b669cc5msh65b1d920849f4ebp157757jsnc5636ee97165",
-            "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
-        }
-    })
-    .then(response => {
-        console.log(response.json());
-        //return response.json();
-        //displayRecipes(response.json());
-    })
-    .catch(err => {
-        console.error(err);
-    });
+//     fetch(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?query=${searchTerm}&number=10&offset=0`, {
+//         "method": "GET",
+//         "headers": {
+//             "x-rapidapi-key": "0f2b669cc5msh65b1d920849f4ebp157757jsnc5636ee97165",
+//             "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+//         }
+//     })
+//     .then(response => {
+//         console.log(response.json());
+//         //return response.json();
+//         //displayRecipes(response.json());
+//     })
+//     .catch(err => {
+//         console.error(err);
+//     });
 
-}
-// searchByIngredient()
-// calls spoonacular api for recipes based on search by ingredients
+// }
+// // searchByIngredient()
+// // calls spoonacular api for recipes based on search by ingredients
 
-//displayRecipes()
-// should be called used getRecipes() and searchByIngredients() , generates cards based on query data and displays image of recipe plus number of used and missing ingredients, clicking on card calls displayRecipeDetails()
+// //displayRecipes()
+// // should be called used getRecipes() and searchByIngredients() , generates cards based on query data and displays image of recipe plus number of used and missing ingredients, clicking on card calls displayRecipeDetails()
 
 
 
-//displayRecipeDetails()
-// generates more detailed description of recipe with image, ingredient lists, and instructions, button with option to open the recipe page in a new window. should also create description for the wine pairing feature with an input field for wine budget and buttons to get wine or skip
+// //displayRecipeDetails()
+// // generates more detailed description of recipe with image, ingredient lists, and instructions, button with option to open the recipe page in a new window. should also create description for the wine pairing feature with an input field for wine budget and buttons to get wine or skip
 
-//getWines()
-//creates apiurl from user input, validates and saves returned object, calls display wine function
+// //getWines()
+// //creates apiurl from user input, validates and saves returned object, calls display wine function
 
-//displayWines()
-// generates a list of possible wine pairings from wineobject with description of pairing choices. generates card(s) with product matches that contain an image, wine rating, and wine price. clicking on card creates modal with description of wine and button to select it
+// //displayWines()
+// // generates a list of possible wine pairings from wineobject with description of pairing choices. generates card(s) with product matches that contain an image, wine rating, and wine price. clicking on card creates modal with description of wine and button to select it
 
-//generateNetflixForm()
-//generates a description of our netflix search criteria and how to utilize the input form. generates form with multiple inputs for Release dates, minimum IMDB rating, actor, move or series toggle, keywords, and genre selector
+// //generateNetflixForm()
+// //generates a description of our netflix search criteria and how to utilize the input form. generates form with multiple inputs for Release dates, minimum IMDB rating, actor, move or series toggle, keywords, and genre selector
 
-//getNetflixResults()
-//generates apiurl from user inputs, validates and saves returned object, calls displayNetflixResults
+// //getNetflixResults()
+// //generates apiurl from user inputs, validates and saves returned object, calls displayNetflixResults
 
-//displayNetflixResults()
-//generates cards with image, movie title, year of release, and imdb rating. clicking on cards creates modal with movie synopsis and button to confirm selection. generates a button below the cards that allows the user to move on to the next set of 5 search results. another button allows the user to return to the netflixForm to start their search over.
+// //displayNetflixResults()
+// //generates cards with image, movie title, year of release, and imdb rating. clicking on cards creates modal with movie synopsis and button to confirm selection. generates a button below the cards that allows the user to move on to the next set of 5 search results. another button allows the user to return to the netflixForm to start their search over.
 
-//summarizeEvening()
-// the final page generates a summary of the dinner, wine, and netflix selections that the user has made so far. clicking on a section provides an overview/summary of the specific option the user selected. button at bottom of the screen returns user to homepage to start over.
+// //summarizeEvening()
+// // the final page generates a summary of the dinner, wine, and netflix selections that the user has made so far. clicking on a section provides an overview/summary of the specific option the user selected. button at bottom of the screen returns user to homepage to start over.
